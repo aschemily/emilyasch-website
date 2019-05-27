4f01c061698500f85749f660ce9934188dc07e23
aschemily <aschemily@gmail.com>
2019-05-27 13:39:44 -0400

    skills

--- src/Skills.js
+++ src/Skills.js
@@ -4,57 +4,55 @@ import { Grid, Segment } from 'semantic-ui-react'
 
 function Skills (){
   return (
-    <Grid>
-    <div id='centered'>
-      <div id='skills'>
+    <Grid columns={3} divided>
+
       <img id='computer' src={require('./computer.jpg')}/>
       <h1 id='skill'>SKILLS</h1>
-      <Grid.Row columns={3}>
+      <Grid.Row>
+        <Grid.Column>
+        <Segment id='circle'>Ruby </Segment>
+        </Grid.Column>
+          <Grid.Column>
+          <Segment id='circle'>Ruby on Rails</Segment>
+          </Grid.Column>
+        <Grid.Column>
+        <Segment id='circle'>JavaScript</Segment>
+        </Grid.Column>
+      </Grid.Row>
 
-      <Grid.Column>
-      <Segment id='circle'>Ruby </Segment>
-      </Grid.Column>
+      <Grid.Row>
+        <Grid.Column>
+        <Segment id='circle'>React</Segment>
+        </Grid.Column>
+          <Grid.Column>
+          <Segment id='circle'>SQL</Segment>
+          </Grid.Column>
+        <Grid.Column>
+        <Segment id='circle'>REST</Segment>
+        </Grid.Column>
+      </Grid.Row>
 
-      <Grid.Column>
-      <Segment id='circle'>Ruby on Rails</Segment>
-      </Grid.Column>
+      <Grid.Row>
+        <Grid.Column>
+        <Segment id='circle'>HTML / HTML5</Segment>
+        </Grid.Column>
 
-      <Grid.Column>
-      <Segment id='circle'>JavaScript</Segment>
-      </Grid.Column>
+        <Grid.Column>
+        <Segment id='circle'>CSS</Segment>
+        </Grid.Column>
 
-      <Grid.Column>
-      <Segment id='circle'>React</Segment>
-      </Grid.Column>
+        <Grid.Column>
+        <Segment id='circle'>Semantic</Segment>
+        </Grid.Column>
 
+      </Grid.Row>
 
-      <Grid.Column>
-      <Segment id='circle'>SQL</Segment>
-      </Grid.Column>
-
-      <Grid.Column>
-      <Segment id='circle'>REST</Segment>
-      </Grid.Column>
-
-      <Grid.Column>
-      <Segment id='circle'>HTML / HTML5</Segment>
-      </Grid.Column>
-
-      <Grid.Column>
-      <Segment id='circle'>CSS</Segment>
-      </Grid.Column>
-
-      <Grid.Column>
-      <Segment id='circle'>Semantic</Segment>
-      </Grid.Column>
-
+      <Grid.Row>
       <Grid.Column>
       <Segment id='circle'>Bootstrap</Segment>
       </Grid.Column>
 
       </Grid.Row>
-      </div>
-    </div>
     </Grid>
   )
 }
